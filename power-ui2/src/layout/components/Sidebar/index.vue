441cc3a8c63fa14ab8057b3fb1193a556167510f
lza <1215046501@qq.com>
2022-11-05 10:27:41 +0800

    前端项目初始化

---
+++ power-ui2/src/layout/components/Sidebar/index.vue
@@ -0,0 +1,61 @@
+<template>
+  <div :class="{'has-logo':showLogo}">
+    <!-- 左边栏的图片和名称显示,关联到Logo.vue -->
+    <logo v-if="showLogo" :collapse="isCollapse" />
+    <!-- el-scrollbar: 滚动事件 -->
+    <el-scrollbar wrap-class="scrollbar-wrapper">
+      <!-- 导航菜单 -->
+      <el-menu
+        :default-active="activeMenu"
+        :collapse="isCollapse"
+        :background-color="variables.menuBg"
+        :text-color="variables.menuText"
+        :unique-opened="false"
+        :active-text-color="variables.menuActiveText"
+        :collapse-transition="false"
+        mode="vertical"
+      >
+        <!--遍历路径-->
+        <sidebar-item v-for="route in routes" :key="route.path" :item="route" :base-path="route.path" />
+      </el-menu>
+    </el-scrollbar>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import Logo from './Logo'
+import SidebarItem from './SidebarItem'
+import variables from '@/styles/variables.scss'
+
+export default {
+  components: { SidebarItem, Logo },
+  computed: {
+    ...mapGetters([
+      'sidebar'
+    ]),
+    routes() {
+      return this.$router.options.routes
+    },
+    activeMenu() {
+      const route = this.$route
+      const { meta, path } = route
+      // if set path, the sidebar will highlight the path you set
+      if (meta.activeMenu) {
+        return meta.activeMenu
+      }
+      return path
+    },
+    showLogo() {
+      // 指向store的state的settings模块的sidebarLogo字段
+      return this.$store.state.settings.sidebarLogo
+    },
+    variables() {
+      return variables
+    },
+    isCollapse() {
+      return !this.sidebar.opened
+    }
+  }
+}
+</script>
---
+++ power-ui2/src/settings.js
@@ -0,0 +1,17 @@
+module.exports = {
+
+  title: 'Vue Admin Template',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  fixedHeader: false,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   * 显示左边栏logo
+   */
+  sidebarLogo: true
+}
